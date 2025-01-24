77e018508ab140e87758965ff156a1221857cef2
Hoanghieutb97 <hoanghieutb97@gmail.com>
2025-01-24 11:24:27 +0700

    svsdv

--- app/page.js
+++ app/page.js
@@ -1,15 +1,10 @@
 export default async function Home() {
-  const res = await fetch("http://localhost:3000/api/data", { cache: "no-store" });
-  const data = await res.json();
+  
 
   return (
     <div>
-      <h1>Danh sách dữ liệu</h1>
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>{item.name}</li> // Thay "name" bằng trường phù hợp trong MongoDB
-        ))}
-      </ul>
+      <h1>trang chủ</h1>
+      
     </div>
   );
 }
